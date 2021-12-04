e9f06ed17d8d9a773c8905579b10198d2562ff48
aravindsiruvuru05 <siruvuruaravind@gmail.com>
2021-12-04 14:07:54 +0530

    footer enhancements

--- src/EmployeePortal/components/Footer/index.jsx
+++ src/EmployeePortal/components/Footer/index.jsx
@@ -4,56 +4,91 @@ import "./styles.css";
 const Footer = () => {
   return (
     <div class="footere">
-    <div class="footer__content">
+      <div class="footer__content">
         <div class="block-3 js--wp-8-2-3">
-            <header>Explore</header>
-           <ul> 
-            <li><a href="">Home</a></li>
-            <li><a href="">Who We Are</a></li>
-            <li><a href="">Curriculum</a></li>
-            <li><a href="">Contact Us</a></li>
-            <li><a href="">Photo Gallery</a></li>
-            <li><a href="">Assignments</a></li>
-            <li><a href="">Newsletter</a></li>
-            <li><a href="">Notes</a></li>
-            <li><a href="">Pollocks App</a></li>
-            <li><a href="">Sitemap</a></li>
-
-            </ul> 
-
+          <header>Explore</header>
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Who We Are</a>
+            </li>
+            <li>
+              <a href="">Curriculum</a>
+            </li>
+            <li>
+              <a href="">Contact Us</a>
+            </li>
+            <li>
+              <a href="">Photo Gallery</a>
+            </li>
+            <li>
+              <a href="">Assignments</a>
+            </li>
+            <li>
+              <a href="">Newsletter</a>
+            </li>
+            <li>
+              <a href="">Notes</a>
+            </li>
+            <li>
+              <a href="">Pollocks App</a>
+            </li>
+            <li>
+              <a href="">Sitemap</a>
+            </li>
+          </ul>
         </div>
         <div class="block-2 js--wp-8-2-2">
           <p class="block-2__header">Brand Name</p>
-            <div class="block-2__icons">
-               asdf
-            </div>
-            <div class="block-2__contact-info">
-              <p>
-              <span>Contact Information</span>
-                <>
-                  Pollocks School  <br/>
-                  Central Office – Dwarakanagar<br/>
-                  Tel. 2754772 / 2750303<br/>
-                  Email. p1@pollocks.in
-                </>
-              </p>
-            </div>
+
+          <ul class="block-2__icons list-inline social-buttons">
+            <li>
+              <a href="#">
+                <i class="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-google"></i>
+              </a>
+            </li>
+          </ul>
+          <div class="block-2__contact-info">
+            <p>
+              <span>Contact Information</span> <br/>
+              <>
+                Pollocks School <br />
+                Central Office – Dwarakanagar
+                <br />
+                Tel. 2754772 / 2750303
+                <br />
+                Email. p1@pollocks.in
+              </>
+            </p>
+          </div>
         </div>
         <div class="block-1 js--wp-8-2-1">
-            <p>
-            <span class="block-1__span">Facilities</span> <br/>
-                State-of-the-art laboratories
-                Fully equipped Computer <br/>
-                Vibrant Library<br/>
-                IT-enabled classrooms<br/>
-                Activity and Resource rooms<br/>
-                Playground and Sports Facilities
-            </p>
+          <p>
+            <span class="block-1__span">Facilities</span> <br />
+            State-of-the-art laboratories Fully equipped Computer <br />
+            Vibrant Library
+            <br />
+            IT-enabled classrooms
+            <br />
+            Activity and Resource rooms
+            <br />
+            Playground and Sports Facilities
+          </p>
         </div>
-        
-       
-    </div>  
-</div> 
+      </div>
+    </div>
   );
 };
 
