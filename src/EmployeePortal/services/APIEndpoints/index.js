26db5e41cc5a2528ac9d35157bf54e43c8fe2c16
prasanth-277 <prasanthreddy549@gmail.com>
2021-12-05 17:26:39 +0530

    build uploaded

--- src/EmployeePortal/services/APIEndpoints/index.js
+++ src/EmployeePortal/services/APIEndpoints/index.js
@@ -3,10 +3,10 @@ const apiEndpoints = {
     "http://localhost:8080/api/jobs/sppublishdesc/true/0/20/posted_on",
   hr_page_jobs: "http://localhost:8080/api/jobs/spdesc/0/20/postedOn",
   home_page_courses:
-    "https://localhost:8080/api/course/sppublishdesc/true/0/20/start_date",
+    "http://localhost:8080/api/course/sppublishdesc/true/0/20/start_date",
   hr_page_courses: "http://localhost:8080/api/course/spase/0/20/startDate",
   hr_create_and_update_job: "http://localhost:8080/api/jobs/",
-  hr_create_and_update_course: "https://localhost:8080/api/course",
+  hr_create_and_update_course: "http://localhost:8080/api/course",
 };
 
 export default apiEndpoints;
