7d292cbcfb877ff58c386d0b1081686bba02183a
prasanth-277 <prasanthreddy549@gmail.com>
2022-01-04 22:11:16 +0530

    build generated

--- src/EmployeePortal/services/APIEndpoints/index.js
+++ src/EmployeePortal/services/APIEndpoints/index.js
@@ -2,27 +2,24 @@ const baseUrl = "http://35.200.162.143:8080/api/course/{id}/";
 
 const apiEndpoints = {
   home_page_jobs: ({ page }) =>
-    `http://35.200.162.143:8080/api/jobs/sppublishdesc/true/${page}/2/posted_on`,
+    `api/jobs/sppublishdesc/true/${page}/2/posted_on`,
   home_page_courses: ({ page }) =>
-    `http://35.200.162.143:8080/api/course/sppublishdesc/true/${page}/2/start_date`,
-  hr_page_jobs: ({ page }) =>
-    `http://35.200.162.143:8080/api/jobs/spdesc/${page}/2/postedOn`,
-  hr_page_courses: ({ page }) =>
-    `http://35.200.162.143:8080/api/course/spase/${page}/2/startDate`,
-  hr_create_and_update_job: "http://35.200.162.143:8080/api/jobs/",
-  hr_create_and_update_course: "http://35.200.162.143:8080/api/course",
+    `api/courses/sppublishdesc/true/${page}/2/start_date`,
+  hr_page_jobs: ({ page }) => `api/jobs/spdesc/${page}/2/postedOn`,
+  hr_page_courses: ({ page }) => `api/courses/spase/${page}/2/startDate`,
+  hr_create_and_update_job: "api/jobs/create",
+  hr_create_and_update_course: "api/courses/create",
   get_employees: ({ page }) => `https://prasanth-277.github.io/employees.json`,
-  hr_page_job_delete: "http://35.200.162.143:8080/api/jobs/",
-  hr_page_course_delete: "http://35.200.162.143:8080/api/course/",
+  hr_page_job_delete: "api/jobs/delete",
+  hr_page_course_delete: "api/courses/delete",
 
-  apply_job: ({ jobId }) => `http://35.200.162.143:8080/api/joback/${jobId}`,
+  apply_job: ({ jobId }) => `api/joback/create/${jobId}`,
   get_job_applications: ({ jobId, page }) =>
-    `http://35.200.162.143:8080/api/joback/spJobAckdesc/${jobId}/${page}/2`,
+    `api/joback/spJobAckdesc/${jobId}/${page}/2`,
 
   get_course_applications: ({ courseId, page }) =>
-    `http://35.200.162.143:8080/api/studentack/spStudentAckdesc/${courseId}/${page}/2`,
-  apply_course: ({ courseId }) =>
-    `http://35.200.162.143:8080/api/studentack/${courseId}`,
+    `api/studentack/spStudentAckdesc/${courseId}/${page}/2`,
+  apply_course: ({ courseId }) => `api/studentack/create/${courseId}`,
 };
 
 export default apiEndpoints;
--- src/EmployeePortal/services/ApiService/api.js
+++ src/EmployeePortal/services/ApiService/api.js
@@ -4,32 +4,33 @@ import axios from "axios";
 import history from "../../routes/history";
 // import createNotification from "../../utils";
 
-const headers = {};
+const headers = {
+  Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJocHVzdWx1cmkiLCJpc0FkbWluIjp0cnVlLCJleHAiOjE2NDEzMzIwMzMsImlhdCI6MTY0MTMxNDAzM30.WMIRKvj5HHIOpxJouEhS3Dtl4TgAt2vKTOwFy4O7vz45ss4dJeTVWcju_Fq8UQRaR47QbWwfy_kO2ZRyeXBcNw`,
+};
 
 const axiosConfig = {
-  baseURL: "https://sureify-portal-be.herokuapp.com/",
+  baseURL: "http://35.200.162.143:8080/",
   headers,
 };
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create(axiosConfig);
 
 axiosConfig.crossdomain = true;
 
-// axiosInstance.interceptors.request.use(function (request) {
-//   if (!window.navigator.onLine) {
-//     createNotification(
-//       'warning',
-//       'Please make sure you have stable internet connection'
-//     );
-//   }
-//   //set access_token if present
-//   const acessToken = localStorage.getItem('access_token');
-//   if (acessToken) {
-//     request.headers.Authorization = `Token ${acessToken}`;
-//   }
+axiosInstance.interceptors.request.use(function (request) {
+  // if (!window.navigator.onLine) {
+  //   createNotification(
+  //     'warning',
+  //     'Please make sure you have stable internet connection'
+  //   );
+  // }
+  //set access_token if present
+  const acessToken =
+    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJocHVzdWx1cmkiLCJpc0FkbWluIjp0cnVlLCJleHAiOjE2NDEzMDc3NzMsImlhdCI6MTY0MTI4OTc3M30.O0GpwuFYwNZQRUyxAJ28X0gqhdhX2KqsCGSo9InHQC0JgAVJVOHiVJ1AyUjQdiuCrlY2Hd7zzKOu97TlRYZzaQ";
+  // request.headers.Authorization = `Bearer ${acessToken}`;
 
-//   return request;
-// });
+  return request;
+});
 
 axiosInstance.interceptors.response.use(
   (response) => {
